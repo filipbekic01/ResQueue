cca6c7c4e921221f837adc3852f2de1d782223f2
Filip Bekic <filip1994sm@gmail.com>
2024-08-05 13:35:59 +0200

    Update

--- backend/Resqueue/Resqueue/obj/Debug/net8.0/Resqueue.AssemblyInfo.cs
+++ backend/Resqueue/Resqueue/obj/Debug/net8.0/Resqueue.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Resqueue")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42481eec9b66422fb624c62ebd3c1ff4853b954b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8bd6a176dc140eed5bb78f23f383232099dac06")]
 [assembly: System.Reflection.AssemblyProductAttribute("Resqueue")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Resqueue")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
