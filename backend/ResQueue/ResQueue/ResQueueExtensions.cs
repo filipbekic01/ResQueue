56440e151c09f68e26dda83b83a2edd2b1daad6b
Filip Bekic <filip1994sm@gmail.com>
2024-10-31 15:37:12 +0100

    Update nuget version

--- backend/ResQueue/ResQueue/ResQueueExtensions.cs
+++ backend/ResQueue/ResQueue/ResQueueExtensions.cs
@@ -43,7 +43,7 @@ public static class ResQueueExtensions
               }
               """
             , "text/javascript"
-        ));
+        )).ExcludeFromDescription();
 
         var rewriteOptions = new RewriteOptions()
             .AddRewrite("^(.*)/$", "$1", false); // Remove trailing slash
