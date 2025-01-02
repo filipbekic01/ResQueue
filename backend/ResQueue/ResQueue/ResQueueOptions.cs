2e2a4a3bf1bed0722a35793e9ffd5211485252ce
cypherix93 <bikram.ghosh93@gmail.com>
2025-01-02 17:07:00 -0500

    added models

--- backend/ResQueue/ResQueue/ResQueueOptions.cs
+++ backend/ResQueue/ResQueue/ResQueueOptions.cs
@@ -1,3 +1,4 @@
+using ResQueue.Dtos.Messages;
 using ResQueue.Enums;
 
 namespace ResQueue
@@ -5,5 +6,6 @@ namespace ResQueue
     public class ResQueueOptions
     {
         public ResQueueSqlEngine SqlEngine { get; set; } = ResQueueSqlEngine.Postgres;
+        public Func<MessageDeliveryDto, Dictionary<string, string>>? AppendAdditionalData { get; set; }
     }
 }
