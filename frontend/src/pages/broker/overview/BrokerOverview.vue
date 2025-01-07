e65e699f264a77f335e200726da0ddc76fc98c4b
Filip Bekic <filip1994sm@gmail.com>
2025-01-07 14:40:29 +0100

    Update

--- frontend/src/pages/broker/overview/BrokerOverview.vue
+++ frontend/src/pages/broker/overview/BrokerOverview.vue
@@ -10,18 +10,22 @@ const data = computed(() => {
     { key: 'Database', value: auth.value?.database },
     { key: 'Schema', value: auth.value?.schema },
     { key: 'Username', value: auth.value?.username },
+    { key: 'Password', value: '*****' },
     { key: 'Port', value: auth.value?.port },
   ]
 })
 </script>
 
 <template>
-  <DataTable :value="data" striped-rows>
+  <DataTable
+    :value="data"
+    striped-rows
+    class="m-4 w-[40rem] overflow-hidden rounded-lg border dark:border-surface-700"
+  >
     <Column header="Property" field="key" class="w-0 text-nowrap"></Column>
     <Column header="Value" field="value"></Column>
   </DataTable>
-
-  <Message severity="secondary" class="m-4 w-[40rem]">
+  <Message severity="secondary" class="m-4 mt-0 w-[40rem]">
     Learn more about SQL transport and configuration in
     <a
       href="https://masstransit.io/documentation/transports/sql"
